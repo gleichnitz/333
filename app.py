769133f551a8786d004c0bdbb8f970ccc5d30a39
jamesaevans <jaevans@princeton.edu>
2014-03-31 11:23:33 -0400

    try to link pages

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def favicon():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-@app.route("/")
+@app.route("/templates")
 def index():
     return render_template('index.html')
 

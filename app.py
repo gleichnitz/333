b8158ff26b0a7d043cde14539a0e1ada5e73495b
jamesaevans <jaevans@princeton.edu>
2014-03-31 12:44:35 -0400

    test

--- app.py
+++ app.py
@@ -40,14 +40,14 @@ def team():
 def grader():
     return render_template('grader.html')
 
-@app.route("/validate.html?ticket=<ticket>")
-def validate():
+# @app.route("/validate.html?ticket=<ticket>")
+# def validate():
     # req = urllib2.Request('https://fed.princeton.edu/cas/serviceValidate?ticket=' + ticket + '?service=http://saltytyga.herokuapp.com/validate.html')
     # response = urllib2.urlopen(req)
     # data = response.read()
     # tree = ET.parse(data)
     # if tree.attrib.contains_key('cas:authenticationSuccess'):  
-    return 'Success'
+  #  return 'Success'
     # else:
       #  return 'Failure'
 

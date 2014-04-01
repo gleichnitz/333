03dc1355151684d1cf5aa1af37615e550c8ce8b0
Vinay <vayyala@princeton.edu>
2014-03-31 20:12:39 -0400

    trying to create session

--- app.py
+++ app.py
@@ -80,5 +80,6 @@ def admin():
 
 # launch
 if __name__ == "__main__":
+    app.secret_key = 'qeqg;abuerjabyekeusxjblelauwbajbhvyhenssj'
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)

d85e1135993e3b9f7e83812f742760d841b55506
Vinay <vayyala@princeton.edu>
2014-04-14 17:10:05 -0400

    fixing broken stuff

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import pickle
-from datatime import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']

c3e7df0a3170790f35bf9d3e18317e34cf3c94ba
jamesaevans <jaevans@princeton.edu>
2014-04-14 00:42:27 -0400

    viewer JS

--- static/js/viewer.js
+++ static/js/viewer.js
@@ -7,7 +7,14 @@ $(document).ready(function(e) {
 		 var href = $(this).find("a").attr("href");
 		 output = href.substring(1, href.length);
 		 $('#codeTitle').text(output);
-		 $('#code1').css("display", "none");
-		 $('#code2').css("display", "initial");
+
+		 if (output == "test.java") {
+		 	$('#test2.java').css("display", "none");
+			$('#test.java').css("display", "initial");
+		 } else {
+		 	$('#test.java').css("display", "none");
+			$('#test2.java').css("display", "initial");
+		 }
+		 
 	});
 });

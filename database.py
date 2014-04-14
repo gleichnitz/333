873ec25a65e1db44edafd6726d06c590638eb86c
Vinay <vayyala@princeton.edu>
2014-04-14 00:58:22 -0400

    db2

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 class Student(db.Model):

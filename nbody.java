0df52a01b4a8fb098172aed744179247bdc0289e
Vinay <vayyala@princeton.edu>
2014-04-14 14:25:55 -0400

    assignments

---
+++ nbody.java
@@ -0,0 +1,6 @@
+public class NBody {
+    private String[] teams; // String array to store names of teams
+    private int[] w, l, r; // arrays to store wins, losses, and remaining games
+    private int[][] g; // stores remaining games between two teams in 2D array
+    private int n; // stores number of teams
+}

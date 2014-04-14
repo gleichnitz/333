c3ba55aa461d5ed7d3bef943ba852972acbfde02
jamesaevans <jaevans@princeton.edu>
2014-04-13 23:34:51 -0400

    viewer JS

--- static/js/viewer.js
+++ static/js/viewer.js
@@ -3,7 +3,7 @@ $(document).ready(function(e) {
 	console.log("bump");
 
 	$('table tr #test.java').click(function() {
-		 $('#codeTitle').text("test!");
-		 console.log("click!");
+		 var href = $(this).find("a").attr("href");
+		 $('#codeTitle').text(href);
 	});
 });

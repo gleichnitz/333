f69ca7729a72cddbd70dfa15b462a610596f4588
Vinay <vayyala@princeton.edu>
2014-04-14 16:57:36 -0400

    fixing broken things 2

---
+++ ass_pop.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import pickle
+from database import *
+
+name = 'Nbody'
+files = []
+nbody_= open('Nbody.java', 'w')
+files.append(nbody_)
+addAssignment('cos333', 'vayyala', name, files)

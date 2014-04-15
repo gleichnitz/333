0baace6b7cd2342bab1b26bbddd5248136a51d22
jamesaevans <jaevans@princeton.edu>
2014-04-15 00:18:33 -0400

    connect pages to database

--- app.py
+++ app.py
@@ -191,8 +191,8 @@ def student():
     # return render_template('student.html', netid=session['username'], classes=, assignments=)
 
     classes = []
-    classes[1] = "COS 126"
-    classes[2] = "COS 226"
+    classes[0] = "COS 126"
+    classes[1] = "COS 226"
 
     return render_template('student.html', netid=session['username'], classes = classes)
 

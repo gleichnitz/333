a32a9f7f026cbabb785c14e8b4e4f89470e51581
jamesaevans <jaevans@princeton.edu>
2014-04-16 09:56:46 -0400

    connect student

--- static/js/viewer3.js
+++ static/js/viewer3.js
@@ -1,6 +1,7 @@
 $(document).ready(function(e) {
-	var firstRef = $('table tr').first().find("a").attr("href");
-	$(firstRef).css("display", "initial");
+	var firstRef = $('table tr').first().find("a"); //.find("a").attr("href");
+	console.log(firstRef.attr("href");
+	//$(firstRef).css("display", "initial");
 	var output = firstRef.substring(1, firstRef.length);
 	$('#codeTitle').text(output);
 	console.log("updated");

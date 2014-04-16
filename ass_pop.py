e849fcb6dbbb92f684ec10e7d113fab8241e5052
Vinay <vayyala@princeton.edu>
2014-04-15 21:27:00 -0400

    bossing up

--- ass_pop.py
+++ ass_pop.py
@@ -4,9 +4,25 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import pickle
 from database import *
 
-name = 'Nbody'
+# name = 'Nbody'
+# files = []
+# nbody_= open('nbody.java', 'r')
+# file_content = nbody_.read()
+# files.append(file_content)
+# addAssignment('cos333', 'vayyala', name, files)
+
+name = 'Assignment1'
 files = []
-nbody_= open('nbody.java', 'r')
+hello_= open('HelloWorld.java', 'r')
 file_content = nbody_.read()
 files.append(file_content)
+
+hifour = open('HiFour.java', 'r')
+file_content = hifour.read()
+files.append(file_content)
+
+sumthree = open('SumThree.java', 'r')
+file_content = sumthree.read()
+files.append(file_content)
+
 addAssignment('cos333', 'vayyala', name, files)

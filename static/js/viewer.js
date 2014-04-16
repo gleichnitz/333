403d94d4386c06cce00a29f67af83f8903f44c4b
jamesaevans <jaevans@princeton.edu>
2014-04-15 23:51:50 -0400

    connect student

--- static/js/viewer.js
+++ static/js/viewer.js
@@ -5,25 +5,4 @@ $(document).ready(function(e) {
 	output = href.substring(1, href.length);
 	$('#codeTitle').text(output);
 	console.log("updated");
-
-	/* $('table tr').click(function() {
-		 var href = $(this).find("a").attr("href");
-		 output = href.substring(1, href.length);
-		 $('#codeTitle').text(output);
-
-		 if (output == "test") {
-		 	$('#test2').css("display", "none");
-		 	$('#test3').css("display", "none");
-			$('#test').css("display", "initial");
-		 } else if (output == "test2") {
-		 	$('#test').css("display", "none");
-		 	$('#test3').css("display", "none");
-			$('#test2').css("display", "initial");
-		 } else {
-		 	$('#test').css("display", "none");
-		 	$('#test2').css("display", "none");
-			$('#test3').css("display", "initial");
-		 }
-		 
-	}); */
 });

4dcf1ce0e33f311869b8534f0b11f97cc4777a01
Vinay <vayyala@princeton.edu>
2014-04-15 21:29:16 -0400

    chill

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * Name: Vinay Ayyala
+ * Login: vayyala
+ * Precept: P02C
+ * 
+ * Outputs the statement "Hello, World"
+ * This is traditionally a person's first program.
+ * 
+ * Compilation: javac HelloWorld.java
+ * Execution: java HelloWorld
+ * 
+ * > java HelloWorld
+ * Hello, World
+ * 
+ * Header is based off of the header from the NameAge.java program 
+ * from www.princeton.edu/~cos126/
+ */
+
+public class HelloWorld {
+ public static void main(String[] args) {
+  System.out.println("Hello, World");
+ }
+}

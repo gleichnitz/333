f34ff525a5bbc06ee28b1395b392fd16b9f960af
jamesaevans <jaevans@princeton.edu>
2014-04-16 17:27:11 -0400

    test

--- app.py
+++ app.py
@@ -241,7 +241,8 @@ def grader():
 
     assignments_form = []
     for item in assignments:
-        assignments_form.append(Assignment(item.id, item.course.name, item.name, item.date.split()[0], item.files, "40/40", item.grader, item.student.netid))
+        if item.grader is netid or item.grader is None:
+            assignments_form.append(Assignment(item.id, item.course.name, item.name, item.date.split()[0], item.files, "40/40", item.grader, item.student.netid))
 
     classes = []
     for item in assignments_form: 

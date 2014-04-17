684c9954e00986aff9d96928227ac53191e8793e
jamesaevans <jaevans@princeton.edu>
2014-04-17 14:12:23 -0400

    ajax

--- static/js/grader.js
+++ static/js/grader.js
@@ -1,13 +1,14 @@
 $(document).ready(function(e) {
 
 	$('td > div > .btn').click(function() {
+		var clickButton = $(this);
 	    console.log("click");
       	$.ajax({
   			url: "/_assign",
   			context: document.body,
   			data: { netid: "test" }
 	  	}).done(function(data) {
-  			$('td > div > .btn').text(data);
+  			clickButton.text(data);
 	  	});
 	});
 

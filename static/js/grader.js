b845b1515a8fff4bac76b47c3f973abebb7c3b21
jamesaevans <jaevans@princeton.edu>
2014-04-17 13:35:25 -0400

    ajax

--- static/js/grader.js
+++ static/js/grader.js
@@ -3,7 +3,7 @@ $(document).ready(function(e) {
     $SCRIPT_ROOT = {{ request.script_root|tojson|safe }};
 
 	$('td > div > .btn').click(function() {
-	   console.log("clicked");
+	   console.log("click");
       $.ajax({
   		url: "/_assign",
   		context: document.body

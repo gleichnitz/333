7ebe0f4ee9f4bc2c9efa15d4463d8d36aebfc21e
jamesaevans <jaevans@princeton.edu>
2014-04-17 12:09:46 -0400

    ajax

--- static/js/grader.js
+++ static/js/grader.js
@@ -1,8 +1,10 @@
 $(document).ready(function(e) {
 
+    $SCRIPT_ROOT = {{ request.script_root|tojson|safe }};
+
 	$('td > div > .btn').click(function() {
 	   console.log("clicked");
-      $.getJSON('/_assign', {
+      $.getJSON($SCRIPT_ROOT + '/_assign', {
         id: "test", //$(this).parent().attr('id'),
         netid: "test" //$('#netid').text()
       }, function(data) {

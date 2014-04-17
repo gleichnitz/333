ff263bbf300f2f2ff54e0de2744bf1ed2037f7ce
jamesaevans <jaevans@princeton.edu>
2014-04-17 11:29:03 -0400

    ajax

--- static/js/grader.js
+++ static/js/grader.js
@@ -1,7 +1,7 @@
 $(document).ready(function(e) {
 
 	$('.assignRow > td > div > .btn btn-default').click(function() {
-      $.getJSON('/_add_numbers', {
+      $.getJSON('/_assign_', {
         id: $(this).parent().attr('id'),
         netid: $('#netid').text()
       }, function(data) {

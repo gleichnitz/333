01c48628b4c708442533f96b24d143a8a953558f
jamesaevans <jaevans@princeton.edu>
2014-04-16 21:32:35 -0400

    test

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ def grader():
     course = grader.course
     assignments = course.assignments.all()
 
-    button_html = "<button type=\"button\" class=\"btn btn-default\">Default</button>"
+    button_html = "<button type=\"button\" class=\"btn btn-default\" style=\"color: black; background-color: white;\">Default</button>"
 
     assignments_form = []
     for item in assignments:

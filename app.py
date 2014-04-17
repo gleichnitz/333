612305c63f876041eb3ec119f5f82f7dde11f550
jamesaevans <jaevans@princeton.edu>
2014-04-17 12:05:25 -0400

    ajax

--- app.py
+++ app.py
@@ -211,11 +211,11 @@ def submitted():
     # render_template('viewer.html', netid = session['username'], assignment=)
     return render_template('viewer.html', netid = netid, assignment = files, title=title)
 
-@app.route('/_assign_')
+@app.route('/_assign')
 def assign_assignment():
-    id = request.args.get('id', 0, type=int)
-    netid = request.args.get('netid')
-    return jsonify(netid)
+    #id = request.args.get('id')
+    #netid = request.args.get('netid')
+    return jsonify("success")
 
     # if assignment is assigned, return false-(netid of grader)
     # if assignment is not assigned, assign to netid and return true-(netid)

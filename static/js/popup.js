a6baf01c2c0fc5e76f84d3e06e2be75118f66e6f
gleichnitz <gtl@princeton.edu>
2014-04-17 20:25:10 -0400

    redo popup

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,30 +1,52 @@
-$(document).ready(
-function deselect() {
-    $(".pop").slideFadeToggle(function() { 
-        $("#contact").removeClass("selected");
-    });    
-}
+$(document).ready(function() {  
 
-$(function() {
-    $("#contact").live('click', function() {
-        if($(this).hasClass("selected")) {
-            deselect();               
-        } else {
-            $(this).addClass("selected");
-            $(".pop").slideFadeToggle(function() { 
-                $("#email").focus();
-            });
-        }
-        return false;
+    //select all the a tag with name equal to modal
+    $('a[name=modal]').click(function(e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        //Get the A tag
+        var id = $(this).attr('href');
+    
+        //Get the screen height and width
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+    
+        //Set height and width to mask to fill up the whole screen
+        $('#mask').css({'width':maskWidth,'height':maskHeight});
+        
+        //transition effect     
+        $('#mask').fadeIn(1000);    
+        $('#mask').fadeTo("slow",0.8);  
+    
+        //Get the window height and width
+        var winH = $(window).height();
+        var winW = $(window).width();
+              
+        //Set the popup window to center
+        $(id).css('top',  winH/2-$(id).height()/2);
+        $(id).css('left', winW/2-$(id).width()/2);
+    
+        //transition effect
+        $(id).fadeIn(2000); 
+    
     });
+    
+    //if close button is clicked
+    $('.window .close').click(function (e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        $('#mask, .window').hide();
+    });     
+    
+    //if mask is clicked
+    $('#mask').click(function () {
+        $(this).hide();
+        $('.window').hide();
+    });         
 
-    $(".close").live('click', function() {
-        deselect();
-        return false;
-    });
+  //id is the ID for the DIV you want to display it as modal window
+  if(e.keyCode == 13) {
+    $('#mask').hide();
+    $('.window').hide();
+  }
 });
-
-$.fn.slideFadeToggle = function(easing, callback) {
-    return this.animate({ opacity: 'toggle', height: 'toggle' }, "fast", easing, callback);
-};​ 
-);

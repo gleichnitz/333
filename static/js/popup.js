2740b64b578ceb4d3b78bf3d50bece0353f16018
gleichnitz <gtl@princeton.edu>
2014-04-17 20:16:06 -0400

    experiment with popup

---
+++ static/js/popup.js
@@ -0,0 +1,30 @@
+$(document).ready(
+function deselect() {
+    $(".pop").slideFadeToggle(function() { 
+        $("#contact").removeClass("selected");
+    });    
+}
+
+$(function() {
+    $("#contact").live('click', function() {
+        if($(this).hasClass("selected")) {
+            deselect();               
+        } else {
+            $(this).addClass("selected");
+            $(".pop").slideFadeToggle(function() { 
+                $("#email").focus();
+            });
+        }
+        return false;
+    });
+
+    $(".close").live('click', function() {
+        deselect();
+        return false;
+    });
+});
+
+$.fn.slideFadeToggle = function(easing, callback) {
+    return this.animate({ opacity: 'toggle', height: 'toggle' }, "fast", easing, callback);
+};​ 
+);

ecea37cb7b0c010e9347db6a133ee6a4f58d61b8
jamesaevans <jaevans@princeton.edu>
2014-04-19 15:34:23 -0400

    grader ajax

--- static/js/grader.js
+++ static/js/grader.js
@@ -9,8 +9,12 @@ $(document).ready(function(e) {
   			context: document.body,
   			data: { netid: $('#netid').text(), id: clickButton.parent().attr('id')}
 	  	}).done(function(data) {
-  			clickButton.css("display", "none");
-  			clickButton.parent().text(data);
+	  		if (data == "success") {
+	  			clickButton.text("Release");
+	  			// Update grader field
+	  		} else {
+	  			clickButton.parent().parent().css('display', 'none')
+	  		}
 	  	});
 	});
 

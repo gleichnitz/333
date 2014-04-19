b79d98972fd0ad161f7ffd4b25abd8af4d6e8513
jamesaevans <jaevans@princeton.edu>
2014-04-19 00:48:17 -0400

    css

--- app.py
+++ app.py
@@ -223,6 +223,16 @@ def submitted():
 def assign_assignment():
     #id = request.args.get('id')
     #netid = request.args.get('netid')
+
+    assignID = request.args.get('id')
+    netid = request.args.get('netid')
+    assign = Assignment.query.filter_by(id = assignID).first()
+    db.session.delete(assign)
+
+    assign.addGrader(netid)
+    db.session.add(assign)
+    db.session.commit()
+
     return request.args.get('id')
 
     # if assignment is assigned, return false-(netid of grader)

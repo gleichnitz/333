1d786498f21dd6b96c597e73242b190d9ca19e3e
jamesaevans <jaevans@princeton.edu>
2014-04-19 00:56:17 -0400

    test

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def assign_assignment():
 
     assignID = request.args.get('id')
     netid = request.args.get('netid')
-    assign = Assignment.query.filter_by(id = assignID).first()
+    #assign = Assignment.query.filter_by(id = assignID).first()
     #db.session.delete(assign)
 
     #assign.addGrader(netid)

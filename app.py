76c77b6c6a6c6652445ff1394d6d6aa83e7ce6d9
jamesaevans <jaevans@princeton.edu>
2014-04-19 01:34:26 -0400

    test

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config.update(
 # print Course.query.all()
 Base = declarative_base()
 
-class Assignment:
+class Assignment2:
     def __init__(self, id, course, name, date, files, grade, grader, student):
         self.id = id
         self.course = course
@@ -226,7 +226,7 @@ def assign_assignment():
 
     # assignID = request.args.get('id')
     # netid = request.args.get('netid')
-    assign = Assignment.query.filter_by(id =4).first()
+    assign = Assignment.query.filter_by(id=4).first()
     #db.session.delete(assign)
 
     #assign.addGrader(netid)
@@ -279,9 +279,9 @@ def grader():
     assignments_form = []
     for item in assignments:
         if item.grader is None:
-            assignments_form.append(Assignment(item.id, item.course.name, item.name, item.date.split()[0], item.files, "40/40", button_html, item.student.netid))
+            assignments_form.append(Assignment2(item.id, item.course.name, item.name, item.date.split()[0], item.files, "40/40", button_html, item.student.netid))
         elif item.grader.netid == netid:
-            assignments_form.append(Assignment(item.id, item.course.name, item.name, item.date.split()[0], item.files, "40/40", item.grader.netid, item.student.netid))
+            assignments_form.append(Assignment2(item.id, item.course.name, item.name, item.date.split()[0], item.files, "40/40", item.grader.netid, item.student.netid))
 
     classes = []
     for item in assignments_form:

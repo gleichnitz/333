53ece27dd73c4874a78c71f1dfdeff4d7f8f80ab
jamesaevans <jaevans@princeton.edu>
2014-04-19 16:26:04 -0400

    grader ajax

--- app.py
+++ app.py
@@ -438,7 +438,7 @@ def admin_students():
     students_form = []
 
     for student in students_db:
-        student_form.append(StudentClass("no name", student.netid))
+        students_form.append(StudentClass("no name", student.netid))
 
     return render_template('admin_students.html', students=students_form, netid=netid, roles = roles)
 

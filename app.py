8af20a8cb1f8f413bf8c25f7b8ee646cbb769494
jamesaevans <jaevans@princeton.edu>
2014-04-19 12:33:24 -0400

    grader ajax

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def assign_assignment():
 
     #assignID = request.args.get('id')
     #netid = request.args.get('netid')
-    assign = Assignment.query.all()
+    assign = Assignment.query.filter_by(netid = "jaevans")
     return "test"
     #return assign
     # if assign.grader is None:

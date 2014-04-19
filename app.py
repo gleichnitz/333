8da0fb9798062e8a6a8c2693c60769347dc9ed6f
jamesaevans <jaevans@princeton.edu>
2014-04-19 14:24:14 -0400

    grader ajax

--- app.py
+++ app.py
@@ -34,6 +34,8 @@ def assign_assignment():
                 if entry.grader is None:
                     try:
                         entry.addGrader(netid)
+                        db.session.delete(assign)
+                        db.session.add(assign)
                         db.session.commit()
                         return netid
                     except:

1da7bfa8a99294234e03a96dcd91e960d105a196
jamesaevans <jaevans@princeton.edu>
2014-04-19 13:39:19 -0400

    grader ajax

--- app.py
+++ app.py
@@ -31,9 +31,11 @@ def assign_assignment():
         return "fail"
     else:
         try:
-            return type(str(test))
+            test2 =  len(test)
         except: 
             return traceback.format_exc()
+
+    return "success"
     #return assign
     # if assign.grader is None:
     #     assign.addGrader(netid)

01191ee13d0284bdaf2f9a446e7d21a18921ba74
jamesaevans <jaevans@princeton.edu>
2014-04-19 02:02:28 -0400

    test

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ def assign_assignment():
         assign.addGrader(netid)
         db.session.add(assign)
         db.session.commit()
-        return "test"
+        return assignID
     else:
         return assign.grader.netid
 

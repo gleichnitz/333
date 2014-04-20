1049d63a350e36ba74a27a0cfc613575ba9c1493
jamesaevans <jaevans@princeton.edu>
2014-04-19 21:21:04 -0400

    grader ajax

--- app.py
+++ app.py
@@ -67,13 +67,15 @@ def add_student():
     netid = str(request.args.get('netid'))
     if netid.isalnum() is False:
         return "false"
+    
+    student = Student.query.filter_by(netid=netid).first();
+    if student is None:
+        cos_333 = Course.query.filter_by(name= 'cos333').first()
+        newStudent = Student("name", "test", netid, cos_333)
+        db.session.add(newStudent)
+        db.session.commit()
 
-    cos_333 = Course.query.filter_by(name= 'cos333').first()
-    newStudent = Student("name", "test", netid, cos_333)
-    db.session.add(newStudent)
-    db.session.commit()
-
-    return "success"
+    return "true"
 
 class AssignmentClass:
     def __init__(self, id, course, name, date, files, grade, grader, student):

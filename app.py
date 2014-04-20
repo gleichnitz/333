0e9993fd3b8c663717d0e265407bc0f7fd268398
Richard Freling <rfreling@princeton.edu>
2014-04-19 21:51:45 -0400

    store annotation work

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def makeRoles(netid):
     return roles
 
 
-@app.route('/store', methods = ['POST'])
+@app.route('/store', methods = ['POST', 'GET'])
 def store():
     array = []
     return jsonify(array)

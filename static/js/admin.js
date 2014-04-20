1206a832a340ed3e66d558672f0ec0b9f56849f9
gleichnitz <gtl@princeton.edu>
2014-04-20 11:54:08 -0400

    adminstudent.js --> admin.js

---
+++ static/js/admin.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+	$('.modal').modal({
+		show: false
+	});
+
+	$('#add_buttons').click(function() {
+		$('.modal').modal('toggle');
+	});
+
+	$('#manual-submit-modal-student').click(function() {
+		var output = $(this).parent().children('input').val();
+		var inputfieldParent = $(this).parent().parent();
+	      	$.ajax({
+	  			url: "/_add_student",
+	  			context: document.body,
+	  			data: { netid: output }
+		  	}).done(function(data) {
+		  		if (data == "true") {
+		  			inputfieldParent.removeClass('has-error');
+		  			inputfieldParent.addClass('has-success');
+		  			console.log(data);
+		  		}
+		  		else {
+		  			inputfieldParent.addClass('has-error');
+		  			inputfieldParent.removeClass('has-success');
+		  			console.log(data);
+		  		}
+		  	});
+	});
+	$('#manual-submit-modal-grader').click(function() {
+		var output = $(this).parent().children('input').val();
+		var inputfieldParent = $(this).parent().parent();
+	      	$.ajax({
+	  			url: "/_add_grader",
+	  			context: document.body,
+	  			data: { name: output }
+		  	}).done(function(data) {
+		  		if (data == "true") {
+		  			inputfieldParent.removeClass('has-error');
+		  			inputfieldParent.addClass('has-success');
+		  			console.log(data);
+		  		}
+		  		else {
+		  			inputfieldParent.addClass('has-error');
+		  			inputfieldParent.removeClass('has-success');
+		  			console.log(data);
+		  		}
+		  	});
+	});
+	$('#manual-submit-modal-assignment').click(function() {
+		var output = $(this).parent().children('input').val();
+		var inputfieldParent = $(this).parent().parent();
+	      	$.ajax({
+	  			url: "/_add_assignment",
+	  			context: document.body,
+	  			data: { netid: output }
+		  	}).done(function(data) {
+		  		if (data == "true") {
+		  			inputfieldParent.removeClass('has-error');
+		  			inputfieldParent.addClass('has-success');
+		  			console.log(data);
+		  		}
+		  		else {
+		  			inputfieldParent.addClass('has-error');
+		  			inputfieldParent.removeClass('has-success');
+		  			console.log(data);
+		  		}
+		  	});
+	});
+
+});

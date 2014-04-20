4a983989b4085570f6fc2f6945e3f83cd0aaa77b
gleichnitz <gtl@princeton.edu>
2014-04-20 09:14:17 -0400

    adminstudent.js new id

--- static/js/adminstudent.js
+++ static/js/adminstudent.js
@@ -29,5 +29,25 @@ $(document).ready(function() {
 		  		console.log(data);
 		  	});
 	});
+	$('#manual-submit-modal').click(function() {
+		var output = $(this).parent().children('input').val();
+		var inputfieldParent = $(this).parent().parent();
+	      	$.ajax({
+	  			url: "/_add_grader",
+	  			context: document.body,
+	  			data: { netid: output }
+		  	}).done(function(data) {
+		  		if (data == "true") {
+		  			inputfieldParent.removeClass('has-error');
+		  			inputfieldParent.addClass('has-success');
+		  			console.log(data);
+		  		}
+		  		else {
+		  			inputfieldParent.addClass('has-error');
+		  			inputfieldParent.removeClass('has-success');
+		  			console.log(data);
+		  		}
+		  	});
+	});
 
 });

02e9390e12542ad85c7de94567032cd2ab83b26c
gleichnitz <gtl@princeton.edu>
2014-04-20 11:52:25 -0400

    adminstudent.js --> admin.js

--- app.py
+++ app.py
@@ -114,6 +114,21 @@ def add_assignment():
 
     return "true"
 
+@app.route('/_remove_student')
+def remove_student():
+    netid = str(request.args.get('netid'))
+    if netid.isalnum() is False:
+        return "false"
+
+    student = Student.query.filter_by(netid=netid).first();
+    if student is None:
+        cos_333 = Course.query.filter_by(name= 'cos333').first()
+        newStudent = Student("name", "test", netid, cos_333)
+        db.session.add(newStudent)
+        db.session.commit()
+
+    return "true"
+
 class AssignmentClass:
     def __init__(self, id, course, name, date, files, grade, grader, student):
         self.id = id

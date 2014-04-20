b44dd3e1a6fee7b2d6942599898438e4200026f9
jamesaevans <jaevans@princeton.edu>
2014-04-20 17:24:38 -0400

    vinays commit

--- app.py
+++ app.py
@@ -419,9 +419,9 @@ def grader():
     assignments_form = []
     for item in assignments:
         if item.grader is None:
-            assignments_form.append(AssignmentClass(item.id, item.course.name, item.name, item.sub_date.split()[0], item.files, "40/40", "None", item.student.netid))
+            assignments_form.append(AssignmentClass(item.id, item.course.name, item.name, "", item.files, "40/40", "None", item.student.netid))
         elif item.grader.netid == netid:
-            assignments_form.append(AssignmentClass(item.id, item.course.name, item.name, item.sub_date.split()[0], item.files, "40/40", item.grader.netid, item.student.netid))
+            assignments_form.append(AssignmentClass(item.id, item.course.name, item.name, "", item.files, "40/40", item.grader.netid, item.student.netid))
 
     classes = []
     for item in assignments_form:

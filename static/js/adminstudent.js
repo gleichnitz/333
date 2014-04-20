474db4d59607467bfcd33c19f04c17f0f8fcbd58
gleichnitz <gtl@princeton.edu>
2014-04-20 09:29:19 -0400

    add assignment not fully functional

--- static/js/adminstudent.js
+++ static/js/adminstudent.js
@@ -35,6 +35,26 @@ $(document).ready(function() {
 	      	$.ajax({
 	  			url: "/_add_grader",
 	  			context: document.body,
+	  			data: { name: output }
+		  	}).done(function(data) {
+		  		if (data == "true") {
+		  			inputfieldParent.removeClass('has-error');
+		  			inputfieldParent.addClass('has-success');
+		  			console.log(data);
+		  		}
+		  		else {
+		  			inputfieldParent.addClass('has-error');
+		  			inputfieldParent.removeClass('has-success');
+		  			console.log(data);
+		  		}
+		  	});
+	});
+	$('#manual-submit-modal-assignment').click(function() {
+		var output = $(this).parent().children('input').val();
+		var inputfieldParent = $(this).parent().parent();
+	      	$.ajax({
+	  			url: "/_add_assignment",
+	  			context: document.body,
 	  			data: { netid: output }
 		  	}).done(function(data) {
 		  		if (data == "true") {

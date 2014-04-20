b1f13c5988236c852e5f3dd0ccb432f6a942b7c7
Richard Freling <rfreling@princeton.edu>
2014-04-19 22:09:00 -0400

    store annotation work

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def makeRoles(netid):
         roles.append("admin")
     return roles
 
-@app.route('/store', methods = ['POST', 'GET'])
+@app.route('/store', methods = ['GET', 'POST'])
 def store():
     array = []
     if request.method == 'GET':

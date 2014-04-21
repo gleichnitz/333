903881cec0feff6095c8838c62911d76056c56e0
Vinay <vayyala@princeton.edu>
2014-04-21 17:25:23 -0400

    hello

--- app.py
+++ app.py
@@ -293,7 +293,7 @@ def create():
     a = Assignment.query.filter_by(id = id).first()
     for i in range(0, len(a.files)):
         if (a.files[i]["name"].split('.')[0] == name):
-            a.files[i]["annotations"].append(request.json)
+            a.files[i]["annotations"].append(str(request.json))
             db.session.commit()
             return json.dumps(len(a.files[i]["annotations"]))
 

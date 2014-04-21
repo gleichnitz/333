6ff8a5363578ba89c460f29c7035a82dbd9c2261
jamesaevans <jaevans@princeton.edu>
2014-04-20 22:43:33 -0400

    saving annotations

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ def store():
     if request.method == 'GET':
         return jsonify(testArray)
     else:
-        testArray.append(requests.json)
+        testArray.append(request.json)
         return jsonify(testArray)
 
 @app.route('/login')

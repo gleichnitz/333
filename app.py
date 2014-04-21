79ab9af6d15c3c78eeb6119a413feae351233e5f
jamesaevans <jaevans@princeton.edu>
2014-04-20 23:07:00 -0400

    saving annotations

--- app.py
+++ app.py
@@ -252,11 +252,8 @@ def makeRoles(netid):
 
 @app.route('/store/annotations', methods = ['GET', 'POST'])
 def store():
-    if request.method == 'GET':
-        return testArray
-    else:
-        testArray.append(request.json)
-        return jsonify(request.json)
+    return jsonify('No JSON payload sent. Annotation not created.',
+                       status=400)
 
 @app.route('/login')
 def login():

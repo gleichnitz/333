59e79adcf979c1a32eefcdf2d1861aa9ef2535cc
gleichnitz <gtl@princeton.edu>
2014-04-22 11:58:36 -0400

    delete aassignment

--- app.py
+++ app.py
@@ -152,6 +152,10 @@ def remove_grader():
 
     return "true"
 
+@app.route("/_delete_assignment")
+def remove_assignment():
+    
+
 @app.route('/admin/students/assignments')
 def admin_studentsdent_assigments:
 

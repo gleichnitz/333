1ad2e008f22532a54084979348b997c7f2e11f88
gleichnitz <gtl@princeton.edu>
2014-04-22 13:34:01 -0400

    remove .first()

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def remove_grader():
 @app.route("/_delete_assignment")
 def remove_assignment():
     name = str(request.args.get('name'))
-    assignments = Assignment.query.filter_by(name=name).first();
+    assignments = Assignment.query.filter_by(name=name);
     for assignment in assignments:
         db.session.delete(assignment)
         db.session.commit()

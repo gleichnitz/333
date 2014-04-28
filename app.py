4ffa10880fd3d7606b8492ea73e56a1e7cd05058
gleichnitz <gtl@princeton.edu>
2014-04-28 18:13:48 -0400

    delete assignments with student

--- app.py
+++ app.py
@@ -236,9 +236,9 @@ def remove_student():
     student = Student.query.filter_by(netid=netid).first();
     if student is None:
         return "true"
-    # assignments = Assignments.query.filter_by(student=student).all();
-    # for assignment in assignments:
-    #     db.session.delete(assignment)
+    assignments = Assignments.query.filter_by(student=student).all();
+    for assignment in assignments:
+        db.session.delete(assignment)
 
     db.session.delete(student)
     db.session.commit()

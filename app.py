365a630397d4690e27626dbecdf34a1a1d23d6e5
Richard Freling <rfreling@princeton.edu>
2014-05-03 10:43:06 -0400

    post grades

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def done():
 
     file_name = ""
     for item in assignment.files:
-        file_name = item.filename
+        file_name = item.get('name')
         file_grade = request.form[file_name]
         assignment.rubric.append(file_grade)
 

2cc786afb1e8db329e743c80b4bc4e6455f3acf8
jamesaevans <jaevans@princeton.edu>
2014-05-05 22:10:27 -0400

    commit

--- app.py
+++ app.py
@@ -98,9 +98,9 @@ def upload_student_files():
     netid = request.form['netid']
 
     # Netid is automatically generated, so it should be valid.
-    #if isValidNetid(netid) is False:
-        #session['error'] = 'unk'
-        #return redirect('/admin/students')
+    if isValidNetid(netid) is False:
+        session['error'] = 'unk'
+        return redirect('/admin/students')
 
     files = request.files.getlist('file')
     string = ""

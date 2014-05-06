b372e44eb26dc62c2c5db15eb8710d4e8d2f7dfd
gleichnitz <gtl@princeton.edu>
2014-05-06 10:59:21 -0400

    removing

--- app.py
+++ app.py
@@ -417,7 +417,7 @@ class AssignmentClass:
         self.points = points
 
 class File:
-    def __init__(self, name, code, grade, totalpoints, isReadOnly = ""):
+    def __init__(self, name, code, grade, isReadOnly = ""):
         self.name = name.split('.')[0]
         if len(name.split('.')) > 1:
             self.ext = name.split('.')[1]
@@ -429,7 +429,6 @@ class File:
             self.ext = "plain"
         self.code = code
         self.grade = grade
-        self.totalpoints = totalpoints
         self.isReadOnly = isReadOnly
 
 class StudentClass:
@@ -749,9 +748,9 @@ def submitted():
     i = 0
     for item in assignment_active.files:
         if accountType == "g":
-            files.append(File(item['name'], item['content'], item['grade'], assignment_active.rubric[i]))
+            files.append(File(item['name'], item['content'], item['grade']))
         else:
-            files.append(File(item['name'], item['content'], item['grade'], assignment_active.rubric[i], "{readOnly: true}"))
+            files.append(File(item['name'], item['content'], item['grade'], "{readOnly: true}"))
         i += 1
 
 

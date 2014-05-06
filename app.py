43d122ac145ac3e3a694c8f68a2fc049c9139796
gleichnitz <gtl@princeton.edu>
2014-05-05 21:23:14 -0400

    check for assignments before deleting student

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ def check_student():
     if student is None:
         return ""
 
-    assignments = Assignment.query.filter_by(student=netid).all()
+    assignments = Assignment.query.filter_by(student=student).all()
     if len(assignments) != 0:
         return "check"
     else:

62f24071c98486703e6392920724ec18cbea9fe1
jamesaevans <jaevans@princeton.edu>
2014-05-06 16:17:20 -0400

    add students properly

--- app.py
+++ app.py
@@ -364,7 +364,9 @@ def check_graded_assignments():
 @app.route('/_add_assignment')
 def add_assignment():
 
-    return redirect('/_add/test/10')
+    session['error'] = 'noname'
+    return "false"
+
     name = request.args.get('name')
     fileNames = request.args.get('files').split()
     rubric = request.args.get('rubric').split()

2978bd4c0fe2ac01b13c4c33b139ef898e37daa1
jamesaevans <jaevans@princeton.edu>
2014-05-07 21:02:00 -0400

    histos

--- static/js/adminstat.js
+++ static/js/adminstat.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	$('.histo').css('display', 'none');
 
 	$('#histoSelect').change(function() {
-		$('#histoContainer').css('display', 'initial')
 		$('.histo').css('display', 'none');
 		$('#highChart' + $(this).val()).css('display', 'initial');
 	});

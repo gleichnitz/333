1345b0b0c056d50813ced38ae09e38421b109f70
jamesaevans <jaevans@princeton.edu>
2014-05-07 20:54:42 -0400

    CAS on inner admin pages

--- static/js/adminstat.js
+++ static/js/adminstat.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	$('.histo').css('display', 'none');
 
 	$('#histoSelect').change(function() {
+		$('#histoContainer').css('display', 'initial')
 		$('.histo').css('display', 'none');
 		$('#highChart' + $(this).val()).css('display', 'initial');
 	});

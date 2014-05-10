7e184875fcaf6b0d82ee03b134c94149541711c6
jamesaevans <jaevans@princeton.edu>
2014-05-09 21:19:35 -0400

    fix bugs

--- static/js/adminstat.js
+++ static/js/adminstat.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
 	$('.histo').css('display', 'none');
 
+	$('#highChart' + $('#histoSelect').val()).css('display', 'initial');
+
 	$('#histoSelect').change(function() {
 		$('.histo').css('display', 'none');
 		$('#highChart' + $(this).val()).css('display', 'initial');

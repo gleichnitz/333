0e07bc996b8c718ffa31667992cd8e3b687db092
jamesaevans <jaevans@princeton.edu>
2014-05-11 20:35:20 -0400

    finalize homepage

--- Hello.java
+++ Hello.java
@@ -1,11 +1,16 @@
 // My first program
-public class Hello {
+public class SumThree {
 
- public static void main(String[] args) {
+    // Print out the sum of three command line integers. 
+    public static void main(String[] args) {
 
-  // Try annotating me!
-  System.out.println("Hello, World");
+        int x=Integer.parseInt(args[0]);
+        int y=Integer.parseInt(args[1]);
+        int z=Integer.parseInt(args[2]);
 
- }
+        int sum=x+y+z;
 
+        System.out.println(x+" + "+y+" + "+z+" = "+sum);
+
+    }
 }
